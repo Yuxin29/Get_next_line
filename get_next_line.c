a605565e92faa84ecccf5c3646a65413c3632f7b
yuxin wu <yuwu@student.hive.fi>
2025-05-16 12:02:30 +0300

    morestructures

--- get_next_line.c
+++ get_next_line.c
@@ -6,47 +6,83 @@
 /*   By: yuwu <yuwu@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 21:19:06 by yuwu              #+#    #+#             */
-/*   Updated: 2025/05/15 14:33:08 by yuwu             ###   ########.fr       */
+/*   Updated: 2025/05/16 12:02:02 by yuwu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_get_line(char	*line_buffer)
+char	*ft_get_buffer(int fd, char *left_c)
 {
-	ft_strjoin(seved, line_buffer);
+	ssize_t bytes_read; //ssize_t: signed versiong of size_t(unsigned)
+	char	*buffer;
+	char    *temp;
+
+    buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	bytes_read = 1;
+    while (!left_c || (!ft_strchr(left_c, '\n') && bytes_read > 0))
+    {
+    	bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			free (buffer);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+    	temp = ft_strjoin(left_c, buffer);
+		if (!temp)
+		{
+			free (buffer);
+			return (NULL);
+		}
+		free(left_c);
+		left_c = temp;
+	}
+	free (buffer);
+    return (left_c);
 }
 
-char	*ft_get_buffer(int fd, char *left_c, char *buffer)
+char	*ft_get_line(int fd)
 {
-	ssize_t	bytes_read;	//ssize_t: signed versiong of size_t(unsigned)
-    	char	*tmp;
+	char	*line;
 
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1))
-    	bytes_read = 1;:
-    	while (!ft_strchr(left_c, '\n') && (bytes_read) > 0)
-    	{
-    		buffer = ft_substr(left_c, 0, 5);
-    	}
-    	ft_strjoin*(
-    	return (buffer);
+	line = ft_get_buffer(fd, NULL);
+	if (!line)
+		return (NULL);
+	return (line);
 }
 
 char	*ft_get_next_line(int fd)
 {
 	static char	*remainder;
-	//Use a static variable remainder to hold leftover data between calls.
-	//Call ft_fill_line_buffer(fd, remainder, buffer) to update remainder with fresh data.
-	//If remainder is NULL (error or EOF), return NULL.
-	//Extract the next line by calling ft_get_line(remainder).
-	//Update remainder by removing the returned line from it (leftover management).:
-	//Return the extracted line.
+	ssize_t		bytes_already_read;
+	char		*buffer;
+	char		*line;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!line_buffer)
+		return (NULL);
+	line = ft_get_line(buffer);
+	bytes_already_read = 1;
+	remainder = ft_get_line(line);
+	while (remainder != NULL)
+	{
+		bytes_already_read = read(fd, buffer, BUFFER_SIZE);
+		remainder = ft_get_line(line);
+	}
+	return (line);
 }
 
-char buffer[BUFFER__SIZE + 1]
+//Use a static variable remainder to hold leftover data between calls.
+//Call ft_get_line(fd, remainder, buffer) to update remainder with fresh data.
+//If remainder is NULL (error or EOF), return NULL.
+//Extract the next line by cleft_calling ft_get_line(remainder).
+//Update remainder by removing the returned line from it (leftover management).:
+//Return the extracted line.
 
 /* Below are notes
-- read from fd until a \n or EOF is found
 - Return each line including the newline.
 - Maintain leftover buffer data between calls(Static variable)
 */

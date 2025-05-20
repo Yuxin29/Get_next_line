d0e5149f79b9aa19e3f5298cd02ad5e9a63ed66a
yuxin wu <yuwu@student.hive.fi>
2025-05-20 21:09:40 +0300

    bonusheader_added

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yuwu <yuwu@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/11 21:24:16 by yuwu              #+#    #+#             */
-/*   Updated: 2025/05/15 13:21:16 by yuwu             ###   ########.fr       */
+/*   Created: 2025/05/20 20:34:35 by yuwu              #+#    #+#             */
+/*   Updated: 2025/05/20 20:35:57 by yuwu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
 # include <fcntl.h>
 # include <unistd.h>
+//# include <limits.h>
 
 #define BUFFER_SIZE 5
 
@@ -27,8 +28,3 @@ char	*ft_strjoin(char *str1, char *str2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
-
-/* check memory links
-cc -Wall -Wextra -Werror 
--fsanitize=address -fsanitize=leak -fsanitize=undefined -ggdb3 *.c && ./a.out
-*/
